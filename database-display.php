559af0ac00bef645aa88c5740d48e457eace3237
nbhastings <62953818+nbhastings@users.noreply.github.com>
2020-04-28 14:46:45 -0400

    Update database-display.php

--- database-display.php
+++ database-display.php
@@ -2,15 +2,47 @@
 	<?php
 		if(isset($_POST['search']))
 		{
-			if(isset($_POST['volleyball'])||isset($_POST['mensbasketball'])) {
+			if(isset($_POST['volleyball'])||isset($_POST['mensbasketball'])||isset($_POST['baseball'])||isset($_POST['crosscountry'])||isset($_POST['cycling'])||isset($_POST['esports'])||isset($_POST['golf'])||isset($_POST['menssoccer'])||isset($_POST['swimming'])||isset($_POST['tennis'])||isset($_POST['trackandfield'])||isset($_POST['triathlon'])||isset($_POST['mensvolleyball'])||isset($_POST['womensbasketball'])||isset($_POST['cheer'])||isset($_POST['dance'])||isset($_POST['womenssoccer'])||isset($_POST['softball'])) {
 				if(isset($_POST['volleyball'])) {$volleyball = $_POST['volleyball'];}
 				else if(empty($_POST['volleyball'])) {$volleyball = 1;}
 				if(isset($_POST['mensbasketball'])) {$mensbasketball = $_POST['mensbasketball'];}
 				else if(empty($_POST['mensbasketball'])) {$mensbasketball = 1;}
+				if(isset($_POST['baseball'])) {$baseball = $_POST['baseball'];}
+				else if(empty($_POST['baseball'])) {$baseball = 1;}
+				if(isset($_POST['crosscountry'])) {$crosscountry = $_POST['crosscountry'];}
+				else if(empty($_POST['crosscountry'])) {$crosscountry = 1;}
+				if(isset($_POST['cycling'])) {$cycling = $_POST['cycling'];}
+				else if(empty($_POST['cycling'])) {$cycling = 1;}
+				if(isset($_POST['esports'])) {$esports = $_POST['esports'];}
+				else if(empty($_POST['esports'])) {$esports = 1;}
+				if(isset($_POST['golf'])) {$golf = $_POST['golf'];}
+				else if(empty($_POST['golf'])) {$golf = 1;}
+				if(isset($_POST['menssoccer'])) {$menssoccer = $_POST['menssoccer'];}
+				else if(empty($_POST['menssoccer'])) {$menssoccer = 1;}
+				if(isset($_POST['swimming'])) {$swimming = $_POST['swimming'];}
+				else if(empty($_POST['swimming'])) {$swimming = 1;}
+				if(isset($_POST['tennis'])) {$tennis = $_POST['tennis'];}
+				else if(empty($_POST['tennis'])) {$tennis = 1;}
+				if(isset($_POST['trackandfield'])) {$trackandfield = $_POST['trackandfield'];}
+				else if(empty($_POST['trackandfield'])) {$trackandfield = 1;}
+				if(isset($_POST['triathlon'])) {$triathlon = $_POST['triathlon'];}
+				else if(empty($_POST['triathlon'])) {$triathlon = 1;}
+				if(isset($_POST['mensvolleyball'])) {$mensvolleyball = $_POST['mensvolleyball'];}
+				else if(empty($_POST['mensvolleyball'])) {$mensvolleyball = 1;}
+				if(isset($_POST['womensbasketball'])) {$womensbasketball = $_POST['womensbasketball'];}
+				else if(empty($_POST['womensbasketball'])) {$womensbasketball = 1;}
+				if(isset($_POST['cheer'])) {$cheer = $_POST['cheer'];}
+				else if(empty($_POST['cheer'])) {$cheer = 1;}
+				if(isset($_POST['dance'])) {$dance = $_POST['dance'];}
+				else if(empty($_POST['dance'])) {$dance = 1;}
+				if(isset($_POST['womenssoccer'])) {$womenssoccer = $_POST['womenssoccer'];}
+				else if(empty($_POST['womenssoccer'])) {$womenssoccer = 1;}
+				if(isset($_POST['softball'])) {$softball = $_POST['softball'];}
+				else if(empty($_POST['softball'])) {$softball = 1;}
 				$valueToSearch = $_POST['valueToSearch'];
 				// search in all table columns
 				// using concat mysql function
-				$query = "SELECT * FROM athletedatabase WHERE CONCAT(fname, lname, sport, date_excused, departure_time) LIKE '%".$valueToSearch."%' AND (sport LIKE '%".$volleyball."%' OR sport LIKE '%".$mensbasketball."%')";
+				$query = "SELECT * FROM athletedatabase WHERE CONCAT(fname, lname, sport, date_excused, departure_time) LIKE '%".$valueToSearch."%' AND (sport LIKE '%".$volleyball."%' OR sport LIKE '%".$mensbasketball."%' OR sport LIKE '%".$baseball."%' OR sport LIKE '%".$crosscountry."%' OR sport LIKE '%".$cycling."%' OR sport LIKE '%".$esports."%' OR sport LIKE '%".$golf."%' OR sport LIKE '%".$menssoccer."%' OR sport LIKE '%".$swimming."%' OR sport LIKE '%".$tennis."%' OR sport LIKE '%".$trackandfield."%' OR sport LIKE '%".$triathlon."%' OR sport LIKE '%".$mensvolleyball."%' OR sport LIKE '%".$womensbasketball."%' OR sport LIKE '%".$cheer."%' OR sport LIKE '%".$dance."%' OR sport LIKE '%".$womenssoccer."%' OR sport LIKE '%".$softball."%')";
 				$search_result = filterTable($query);
 			} else {
 				$valueToSearch = $_POST['valueToSearch'];
